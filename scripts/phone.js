8821974b42dfaf288346364c18c9b51031596ef6
sh4aadman <sh4adman@gmail.com>
2023-09-08 16:07:57 +0600

    added header section

--- scripts/phone.js
+++ scripts/phone.js
@@ -3,7 +3,7 @@ const loadPhone = async () => {
     const data = await response.json();
     const phones = data.data;
     displayPhones(phones);
-}
+};
 
 const displayPhones = (phones) => {
     const phoneContainer = document.getElementById('phone-container');
@@ -21,13 +21,17 @@ const displayPhones = (phones) => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+                <button class="btn bg-primary">Buy Now</button>
             </div>
         </div>
         `;
 
         phoneContainer.appendChild(phoneCard);
     })
-}
+};
+
+const searchPhone = () => {
+    
+};
 
 loadPhone();
